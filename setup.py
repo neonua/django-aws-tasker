73b5c2aaa02c4962bf0c5d9164790bd1b354a0da
neonua666 <igor.g@enkonix.com>
2021-09-23 13:00:27 +0300

    test commit

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 
 def get_version(*file_paths):
-    """Retrieves the version from lambda-django-tasker/__init__.py"""
+    """Retrieves the version from lambda_django_tasker/__init__.py"""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
@@ -20,7 +20,7 @@ def get_version(*file_paths):
     raise RuntimeError('Unable to find version string.')
 
 
-version = get_version("lambda-django-tasker", "__init__.py")
+version = get_version("lambda_django_tasker", "__init__.py")
 
 if sys.argv[-1] == 'publish':
     try:
